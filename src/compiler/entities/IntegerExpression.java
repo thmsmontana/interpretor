710bb60b9c0c7213cd8cd0c84bb437cbcd6e745c
Quentin Bitschene <quentin.bitschene@gmail.com>
2014-05-10 03:14:06 +0700

    Ajoute les definitions de integer et string expression

--- src/compiler/entities/IntegerExpression.java
+++ src/compiler/entities/IntegerExpression.java
@@ -4,4 +4,229 @@ public class IntegerExpression extends Expression {
     protected IntegerExpression(Type type, Object value, boolean isFormal) {
         super(type, value, isFormal);
     }
+    
+    /* Defining operations */
+	public Expression applyEQUAL(Expression rightOperand) throws Exception {
+		if (rightOperand.getType() == Type.FLOAT) {
+                    if (!this.isFormal && !rightOperand.isFormal) {
+			Float f = (Float) rightOperand.getValue();
+                        this.value = f.intValue();
+                        return Expression.createExpression(Type.INTEGER, value);
+                    }
+                    return Expression.createParameter(Type.INTEGER);
+                }
+		
+                return super.applyEQUAL(rightOperand);
+	}
+
+	public Expression applyEQ(Expression rightOperand) throws Exception {
+		
+                if(rightOperand.getType() == Type.FLOAT)
+                {
+                     if (!this.isFormal && !rightOperand.isFormal) {
+			Float f = (Float) rightOperand.getValue();
+                        return Expression.createExpression(Type.BOOLEAN, (int)this.value==f);
+                    }
+                } 
+                
+                return super.applyEQ(rightOperand);
+                
+	}
+
+	public Expression applyNE(Expression rightOperand) throws Exception {
+		
+                if(rightOperand.getType() == Type.FLOAT)
+                {
+                     if (!this.isFormal && !rightOperand.isFormal) {
+			Float f = (Float) rightOperand.getValue();
+                        return Expression.createExpression(Type.BOOLEAN, (int)this.value!=f);
+                    }
+                } 
+                
+                return super.applyNE(rightOperand);
+	}
+
+	public Expression applyLT(Expression rightOperand) throws Exception {
+            
+		if(rightOperand.getType() == Type.FLOAT)
+                {
+                     if (!this.isFormal && !rightOperand.isFormal) {
+			Float f = (Float) rightOperand.getValue();
+                        return Expression.createExpression(Type.BOOLEAN, (int)this.value<f);
+                    }
+                } 
+                
+                if(rightOperand.getType() == Type.INTEGER)
+                {
+                     if (!this.isFormal && !rightOperand.isFormal) {
+			Integer f = (Integer) rightOperand.getValue();
+                        return Expression.createExpression(Type.BOOLEAN, (int)this.value<f);
+                    }
+                } 
+                
+                throw new Exception("Right operand type is "+rightOperand.getType()+ " must be int or float.");
+                
+                
+	}
+
+	public Expression applyLE(Expression rightOperand) throws Exception {
+		if(rightOperand.getType() == Type.FLOAT)
+                {
+                     if (!this.isFormal && !rightOperand.isFormal) {
+			Float f = (Float) rightOperand.getValue();
+                        return Expression.createExpression(Type.BOOLEAN, (int)this.value<=f);
+                    }
+                } 
+                
+                if(rightOperand.getType() == Type.INTEGER)
+                {
+                     if (!this.isFormal && !rightOperand.isFormal) {
+			Integer f = (Integer) rightOperand.getValue();
+                        return Expression.createExpression(Type.BOOLEAN, (int)this.value<=f);
+                    }
+                } 
+                
+                throw new Exception("Right operand type is "+rightOperand.getType()+ " must be int or float.");
+	}
+
+	public Expression applyGT(Expression rightOperand) throws Exception {
+		if(rightOperand.getType() == Type.FLOAT)
+                {
+                     if (!this.isFormal && !rightOperand.isFormal) {
+			Float f = (Float) rightOperand.getValue();
+                        return Expression.createExpression(Type.BOOLEAN, (int)this.value>f);
+                    }
+                } 
+                
+                if(rightOperand.getType() == Type.INTEGER)
+                {
+                     if (!this.isFormal && !rightOperand.isFormal) {
+			Integer f = (Integer) rightOperand.getValue();
+                        return Expression.createExpression(Type.BOOLEAN, (int)this.value>f);
+                    }
+                } 
+                
+                throw new Exception("Right operand type is "+rightOperand.getType()+ " must be int or float.");
+	}
+
+	public Expression applyGE(Expression rightOperand) throws Exception {
+		if(rightOperand.getType() == Type.FLOAT)
+                {
+                     if (!this.isFormal && !rightOperand.isFormal) {
+			Float f = (Float) rightOperand.getValue();
+                        return Expression.createExpression(Type.BOOLEAN, (int)this.value>=f);
+                    }
+                } 
+                
+                if(rightOperand.getType() == Type.INTEGER)
+                {
+                     if (!this.isFormal && !rightOperand.isFormal) {
+			Integer f = (Integer) rightOperand.getValue();
+                        return Expression.createExpression(Type.BOOLEAN, (int)this.value>=f);
+                    }
+                } 
+                
+                throw new Exception("Right operand type is "+rightOperand.getType()+ " must be int or float.");
+	}
+
+	public Expression applyPLUS(Expression rightOperand) throws Exception {
+		if(rightOperand.getType() == Type.FLOAT)
+                {
+                     if (!this.isFormal && !rightOperand.isFormal) {
+			Float f = (Float) rightOperand.getValue();
+                        return Expression.createExpression(Type.FLOAT, (int)this.value+f);
+                    }
+                } 
+                
+                if(rightOperand.getType() == Type.INTEGER)
+                {
+                     if (!this.isFormal && !rightOperand.isFormal) {
+			Integer f = (Integer) rightOperand.getValue();
+                        return Expression.createExpression(Type.INTEGER, (int)this.value+f);
+                    }
+                } 
+                
+                throw new Exception("Right operand type is "+rightOperand.getType()+ " must be int or float.");
+	}
+
+	public Expression applyMINUS(Expression rightOperand) throws Exception {
+		if(rightOperand.getType() == Type.FLOAT)
+                {
+                     if (!this.isFormal && !rightOperand.isFormal) {
+			Float f = (Float) rightOperand.getValue();
+                        return Expression.createExpression(Type.FLOAT, (int)this.value-f);
+                    }
+                } 
+                
+                if(rightOperand.getType() == Type.INTEGER)
+                {
+                     if (!this.isFormal && !rightOperand.isFormal) {
+			Integer f = (Integer) rightOperand.getValue();
+                        return Expression.createExpression(Type.INTEGER, (int)this.value-f);
+                    }
+                } 
+                
+                throw new Exception("Right operand type is "+rightOperand.getType()+ " must be int or float.");
+	}
+
+	public Expression applyTIMES(Expression rightOperand) throws Exception {
+		if(rightOperand.getType() == Type.FLOAT)
+                {
+                     if (!this.isFormal && !rightOperand.isFormal) {
+			Float f = (Float) rightOperand.getValue();
+                        return Expression.createExpression(Type.FLOAT, (int)this.value*f);
+                    }
+                } 
+                
+                if(rightOperand.getType() == Type.INTEGER)
+                {
+                     if (!this.isFormal && !rightOperand.isFormal) {
+			Integer f = (Integer) rightOperand.getValue();
+                        return Expression.createExpression(Type.INTEGER, (int)this.value*f);
+                    }
+                } 
+                
+                throw new Exception("Right operand type is "+rightOperand.getType()+ " must be int or float.");
+	}
+
+	public Expression applyDIV(Expression rightOperand) throws Exception {
+		if(rightOperand.getType() == Type.FLOAT)
+                {
+                     if (!this.isFormal && !rightOperand.isFormal) {
+			Float f = (Float) rightOperand.getValue();
+                        return Expression.createExpression(Type.FLOAT, (int)this.value/f);
+                    }
+                } 
+                
+                if(rightOperand.getType() == Type.INTEGER)
+                {
+                     if (!this.isFormal && !rightOperand.isFormal) {
+			Integer f = (Integer) rightOperand.getValue();
+                        return Expression.createExpression(Type.INTEGER, (int)this.value/f);
+                    }
+                } 
+                
+                throw new Exception("Right operand type is "+rightOperand.getType()+ " must be int or float.");
+	}
+
+	public Expression applyMOD(Expression rightOperand) throws Exception {
+		if(rightOperand.getType() == Type.FLOAT)
+                {
+                     if (!this.isFormal && !rightOperand.isFormal) {
+			Float f = (Float) rightOperand.getValue();
+                        return Expression.createExpression(Type.FLOAT, (int)this.value%f);
+                    }
+                } 
+                
+                if(rightOperand.getType() == Type.INTEGER)
+                {
+                     if (!this.isFormal && !rightOperand.isFormal) {
+			Integer f = (Integer) rightOperand.getValue();
+                        return Expression.createExpression(Type.INTEGER, (int)this.value%f);
+                    }
+                } 
+                
+                throw new Exception("Right operand type is "+rightOperand.getType()+ " must be int or float.");
+	}
+
 }
--- src/compiler/entities/StringExpression.java
+++ src/compiler/entities/StringExpression.java
@@ -4,4 +4,17 @@ public class StringExpression extends Expression {
     protected StringExpression(Type type, Object value, boolean isFormal) {
         super(type, value, isFormal);
     }
+    
+    /* Defining operations */
+
+	public Expression applyPLUS(Expression rightOperand) throws Exception {
+		if(rightOperand.getType()!=Type.STRING)
+                    throw new Exception("Right operand type is "+rightOperand.getType()+ " must be String.");
+                
+                if(this.isFormal || rightOperand.isFormal)
+                    return Expression.createParameter(type);
+                String result = (String) this.value+(String)rightOperand.value;
+                
+                return Expression.createExpression(Type.STRING, result);
+	}
 }
