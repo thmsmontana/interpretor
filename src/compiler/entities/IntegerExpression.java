136c906629d33386a03f74672c63b56f69c59831
Marquez Nicolas <nico_mhr@hotmail.fr>
2014-05-10 02:00:08 +0700

    derived expressions

---
+++ src/compiler/entities/IntegerExpression.java
@@ -0,0 +1,7 @@
+package compiler.entities;
+
+public class IntegerExpression extends Expression {
+    protected IntegerExpression(Type type, Object value, boolean isFormal) {
+        super(type, value, isFormal);
+    }
+}
---
+++ src/compiler/entities/StringExpression.java
@@ -0,0 +1,7 @@
+package compiler.entities;
+
+public class StringExpression extends Expression {
+    protected StringExpression(Type type, Object value, boolean isFormal) {
+        super(type, value, isFormal);
+    }
+}
