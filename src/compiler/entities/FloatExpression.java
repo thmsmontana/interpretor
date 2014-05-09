591fc8e7299d6cbbab8100ebc83b77fce96e495b
Thomas Montana <thmsmontana@gmail.com>
2014-05-10 03:39:24 +0700

    FloatExpression

--- src/compiler/entities/FloatExpression.java
+++ src/compiler/entities/FloatExpression.java
@@ -1,78 +1,232 @@
 package compiler.entities;
 
-public class FloatExpression extends Expression {
-    protected FloatExpression(Type type, Object value, boolean isFormal) {
-        super(type, value, isFormal);
-    }
-    
+public class IntegerExpression extends Expression {
+	protected IntegerExpression(Type type, Object value, boolean isFormal) {
+		super(type, value, isFormal);
+	}
+
 	/* Defining operations */
 	public Expression applyEQUAL(Expression rightOperand) throws Exception {
-		if (this.getType() != rightOperand.getType()) {
-			throw new Exception("Could not cast " + rightOperand.getType() + " into " this.getType() + ".");
+		if (rightOperand.getType() == Type.INTEGER) {
+			if (!this.isFormal && !rightOperand.isFormal) {
+				Integer f = (Integer) rightOperand.getValue();
+				this.value = f.floatValue();
+				return Expression.createExpression(Type.FLOAT, value);
+			}
+			return Expression.createParameter(Type.FLOAT);
 		}
 		
-		if (!this.isFormal && !rightOperand.isFormal) {
-			this.value = rightOperand.getValue();
-		}
-		return new Expression(type, value, isFormal);
-	}
-
-	public Expression applyOR(Expression rightOperand) throws Exception {
-		throw new Exception("Operation OR is not permitted for type " + this.getType() + ".");
-	}
-
-	public Expression applyAND(Expression rightOperand) throws Exception {
-		throw new Exception("Operation AND is not permitted for type " + this.getType() + ".");
+		return super.applyEQUAL(rightOperand);
 	}
 
 	public Expression applyEQ(Expression rightOperand) throws Exception {
-		throw new Exception("Operation EQ is not permitted for type " + this.getType() + ".");
+		
+		if(rightOperand.getType() == Type.INTEGER)
+		{
+			if (!this.isFormal && !rightOperand.isFormal) {
+				Integer f = (Integer) rightOperand.getValue();
+				return Expression.createExpression(Type.BOOLEAN, (float)this.value==f);
+			}
+		}
+
+		return super.applyEQ(rightOperand);
+
 	}
 
 	public Expression applyNE(Expression rightOperand) throws Exception {
-		throw new Exception("Operation NE is not permitted for type " + this.getType() + ".");
+		
+		if(rightOperand.getType() == Type.INTEGER)
+		{
+			if (!this.isFormal && !rightOperand.isFormal) {
+				Integer f = (Integer) rightOperand.getValue();
+				return Expression.createExpression(Type.BOOLEAN, (float)this.value!=f);
+			}
+		} 
+
+		return super.applyNE(rightOperand);
 	}
 
 	public Expression applyLT(Expression rightOperand) throws Exception {
-		throw new Exception("Operation LT is not permitted for type " + this.getType() + ".");
+
+		if(rightOperand.getType() == Type.INTEGER)
+		{
+			if (!this.isFormal && !rightOperand.isFormal) {
+				Integer f = (Integer) rightOperand.getValue();
+				return Expression.createExpression(Type.BOOLEAN, (float)this.value<f);
+			}
+		} 
+
+		if(rightOperand.getType() == Type.FLOAT)
+		{
+			if (!this.isFormal && !rightOperand.isFormal) {
+				Float f = (Float) rightOperand.getValue();
+				return Expression.createExpression(Type.BOOLEAN, (float)this.value<f);
+			}
+		} 
+
+		throw new Exception("Right operand type is "+rightOperand.getType()+ " must be int or float.");
+
+
 	}
 
 	public Expression applyLE(Expression rightOperand) throws Exception {
-		throw new Exception("Operation LE is not permitted for type " + this.getType() + ".");
+		if(rightOperand.getType() == Type.INTEGER)
+		{
+			if (!this.isFormal && !rightOperand.isFormal) {
+				Integer f = (Integer) rightOperand.getValue();
+				return Expression.createExpression(Type.BOOLEAN, (float)this.value<=f);
+			}
+		} 
+
+		if(rightOperand.getType() == Type.FLOAT)
+		{
+			if (!this.isFormal && !rightOperand.isFormal) {
+				Float f = (Float) rightOperand.getValue();
+				return Expression.createExpression(Type.BOOLEAN, (float)this.value<=f);
+			}
+		} 
+
+		throw new Exception("Right operand type is "+rightOperand.getType()+ " must be int or float.");
 	}
 
 	public Expression applyGT(Expression rightOperand) throws Exception {
-		throw new Exception("Operation GT is not permitted for type " + this.getType() + ".");
+		if(rightOperand.getType() == Type.INTEGER)
+		{
+			if (!this.isFormal && !rightOperand.isFormal) {
+				Integer f = (Integer) rightOperand.getValue();
+				return Expression.createExpression(Type.BOOLEAN, (float)this.value>f);
+			}
+		} 
+
+		if(rightOperand.getType() == Type.FLOAT)
+		{
+			if (!this.isFormal && !rightOperand.isFormal) {
+				Float f = (Float) rightOperand.getValue();
+				return Expression.createExpression(Type.BOOLEAN, (float)this.value>f);
+			}
+		} 
+
+		throw new Exception("Right operand type is "+rightOperand.getType()+ " must be int or float.");
 	}
 
 	public Expression applyGE(Expression rightOperand) throws Exception {
-		throw new Exception("Operation GE is not permitted for type " + this.getType() + ".");
+		if(rightOperand.getType() == Type.INTEGER)
+		{
+			if (!this.isFormal && !rightOperand.isFormal) {
+				Integer f = (Integer) rightOperand.getValue();
+				return Expression.createExpression(Type.BOOLEAN, (float)this.value>=f);
+			}
+		} 
+
+		if(rightOperand.getType() == Type.FLOAT)
+		{
+			if (!this.isFormal && !rightOperand.isFormal) {
+				Float f = (Float) rightOperand.getValue();
+				return Expression.createExpression(Type.BOOLEAN, (float)this.value>=f);
+			}
+		} 
+
+		throw new Exception("Right operand type is "+rightOperand.getType()+ " must be int or float.");
 	}
 
 	public Expression applyPLUS(Expression rightOperand) throws Exception {
-		throw new Exception("Operation PLUS is not permitted for type " + this.getType() + ".");
+		if(rightOperand.getType() == Type.INTEGER)
+		{
+			if (!this.isFormal && !rightOperand.isFormal) {
+				Integer f = (Integer) rightOperand.getValue();
+				return Expression.createExpression(Type.FLOAT, (float)this.value+f);
+			}
+		} 
+
+		if(rightOperand.getType() == Type.FLOAT)
+		{
+			if (!this.isFormal && !rightOperand.isFormal) {
+				Float f = (Float) rightOperand.getValue();
+				return Expression.createExpression(Type.FLOAT, (float)this.value+f);
+			}
+		} 
+
+		throw new Exception("Right operand type is "+rightOperand.getType()+ " must be int or float.");
 	}
 
 	public Expression applyMINUS(Expression rightOperand) throws Exception {
-		throw new Exception("Operation MINUS is not permitted for type " + this.getType() + ".");
+		if(rightOperand.getType() == Type.INTEGER)
+		{
+			if (!this.isFormal && !rightOperand.isFormal) {
+				Integer f = (Integer) rightOperand.getValue();
+				return Expression.createExpression(Type.FLOAT, (float)this.value-f);
+			}
+		} 
+
+		if(rightOperand.getType() == Type.FLOAT)
+		{
+			if (!this.isFormal && !rightOperand.isFormal) {
+				Float f = (Float) rightOperand.getValue();
+				return Expression.createExpression(Type.FLOAT, (float)this.value-f);
+			}
+		}
+
+		throw new Exception("Right operand type is "+rightOperand.getType()+ " must be int or float.");
 	}
 
 	public Expression applyTIMES(Expression rightOperand) throws Exception {
-		throw new Exception("Operation TIMES is not permitted for type " + this.getType() + ".");
+		if(rightOperand.getType() == Type.INTEGER)
+		{
+			if (!this.isFormal && !rightOperand.isFormal) {
+				Integer f = (Integer) rightOperand.getValue();
+				return Expression.createExpression(Type.FLOAT, (float)this.value*f);
+			}
+		} 
+
+		if(rightOperand.getType() == Type.FLOAT)
+		{
+			if (!this.isFormal && !rightOperand.isFormal) {
+				Float f = (Float) rightOperand.getValue();
+				return Expression.createExpression(Type.FLOAT, (float)this.value*f);
+			}
+		} 
+
+		throw new Exception("Right operand type is "+rightOperand.getType()+ " must be int or float.");
 	}
 
 	public Expression applyDIV(Expression rightOperand) throws Exception {
-		throw new Exception("Operation DIV is not permitted for type " + this.getType() + ".");
+		if(rightOperand.getType() == Type.INTEGER)
+		{
+			if (!this.isFormal && !rightOperand.isFormal) {
+				Integer f = (Integer) rightOperand.getValue();
+				return Expression.createExpression(Type.FLOAT, (float)this.value/f);
+			}
+		} 
+
+		if(rightOperand.getType() == Type.FLOAT)
+		{
+			if (!this.isFormal && !rightOperand.isFormal) {
+				Float f = (Float) rightOperand.getValue();
+				return Expression.createExpression(Type.FLOAT, (float)this.value/f);
+			}
+		} 
+
+		throw new Exception("Right operand type is "+rightOperand.getType()+ " must be int or float.");
 	}
 
 	public Expression applyMOD(Expression rightOperand) throws Exception {
-		throw new Exception("Operation MOD is not permitted for type " + this.getType() + ".");
+		if(rightOperand.getType() == Type.INTEGER)
+		{
+			if (!this.isFormal && !rightOperand.isFormal) {
+				Integer f = (Integer) rightOperand.getValue();
+				return Expression.createExpression(Type.FLOAT, (float)this.value%f);
+			}
+		} 
+
+		if(rightOperand.getType() == Type.FLOAT)
+		{
+			if (!this.isFormal && !rightOperand.isFormal) {
+				Float f = (Float) rightOperand.getValue();
+				return Expression.createExpression(Type.FLOAT, (float)this.value%f);
+			}
+		} 
+
+		throw new Exception("Right operand type is "+rightOperand.getType()+ " must be int or float.");
 	}
 
-	public Expression applyQUES(Expression trueOperand, Expression falseOperand) throws Exception {
-		throw new Exception("The condition for a ternary expression should be a boolean.");
-	}
-	public Expression applyEXCL(Expression operand) throws Exception {
-		throw new Exception("Operation EXCL is not permitted for type " + this.getType() + ".");
-	}
 }
